d716968ff954684709a97a928a83a5875e3e56df
Sportsguy <mahoward2s@gmail.com>
2021-08-05 08:25:34 -0500

    setting up deployment before I begin working on it

---
+++ 6crossesvault/src/components/footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+  <div class="footer">
+    Website By: Mahoward2s
+  </div>
+  );
+}
+
+export default Footer;
